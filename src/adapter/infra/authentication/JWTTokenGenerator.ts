cb87b9d9874394473d329b9d8c908e8fcbff1abd
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-11 21:39:14 -0300

    feat: add jwt token generator

---
+++ src/adapter/infra/authentication/JWTTokenGenerator.ts
@@ -0,0 +1,15 @@
+import { sign } from 'jsonwebtoken'
+import { TokenGenerator } from '../../../application/ports/out/TokenGenerator'
+import { secret } from '../../../config/secret'
+
+
+export class JWTTokenGenerator implements TokenGenerator {
+
+    async generate(username: string): Promise<string> {
+        if(!secret) {
+            throw Error('Missing environment variable JSON_WEB_TOKEN_SECRET')
+        }
+
+        return sign({username}, secret, {expiresIn: '1h'})
+    }
+}
