015fe62e69e413e78fbc0c279336d135e8b724b1
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-11 21:39:30 -0300

    feat: add match method

--- src/adapter/infra/repository/InMemoryUserRepository.ts
+++ src/adapter/infra/repository/InMemoryUserRepository.ts
@@ -15,6 +15,11 @@ export class InMemoryUserRepository implements UserRepository {
         this.items.push(user)
     }
 
+    async match(user: User): Promise<boolean> {
+        return this.items
+        .filter(eachUser => eachUser.getEmail() === user.getEmail() && eachUser.getPassword() === user.getPassword()).length > 0
+    }
+
     private async existsByEmail(email: string): Promise<boolean> {
         return this.items.filter(each => each.getEmail() === email).length > 0
     }
