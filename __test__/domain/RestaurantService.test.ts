3c7e689d935a1d3415d57964404c1c02f9b9e7b2
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-15 13:45:19 -0300

    test: teste create service method

---
+++ __test__/domain/RestaurantService.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { InMemoryRestaurantRepository } from '../../src/adapter/infra/repository/InMemoryRestaurantRepository'
+import { RestaurantCreateDTO } from '../../src/application/dto/RestaurantDTO'
+import { RestaurantService } from '../../src/application/ports/in/RestaurantService'
+import { RestaurantServiceImpl } from '../../src/application/service/RestaurantServiceImpl'
+
+describe('Restaurant use cases', () => {
+
+    const restaurantRepository = new InMemoryRestaurantRepository()
+    const restaurantService: RestaurantService = new RestaurantServiceImpl(restaurantRepository)
+
+    beforeEach(() => {
+        restaurantRepository.items = new Array()
+    })
+
+    it('should create a new restaurant', async () => {
+        const restaurantCreateDTO: RestaurantCreateDTO = {
+            name: "Great Restaurant",
+            address: {
+                streetName:"Restaurant street",
+                zipCode: "123321",
+                cityName: "Restaurant city",
+                stateName: "RS"
+            }
+        }
+
+        const createdRestaurant = await restaurantService.register(restaurantCreateDTO)
+
+        expect(restaurantRepository.items.length).toBe(1)
+        expect(createdRestaurant.id).toBeTruthy()
+        expect(createdRestaurant.meals.length).toBe(0)
+
+    })
+})
