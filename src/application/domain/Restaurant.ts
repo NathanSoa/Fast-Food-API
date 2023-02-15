7b45cdc6c97d5109e196d878de3a9b9e8541c0da
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-15 13:45:05 -0300

    refactor: remove getters and setters

--- src/application/domain/Restaurant.ts
+++ src/application/domain/Restaurant.ts
@@ -10,43 +10,15 @@ type restaurantProperties = {
 Error
 export class Restaurant {
 
+    id: string
+    name: string
+    address: Address
+    meals: Meal[]
+    
     constructor(props: restaurantProperties, id?: string) {
         this.id = id || uuid()
         this.name = props.name
         this.address = props.address
         this.meals = props.meals
-    }
-
-    get id(): string {
-        return this.id
-    }
-
-    get name(): string {
-        return this.name
-    }
-
-    get address(): Address {
-        return this.address
-    }
-
-    get meals(): Meal[] {
-        return this.meals
-    }
-
-    
-    set id(id: string) {
-        this.id = id
-    }
-
-    set name(name: string) {
-        this.name = name
-    }
-
-    set address(address: Address) {
-        this.address = address
-    }
-
-    set meals(meals: Meal[]) {
-        this.meals = meals
     }
 }
