8f646a46a7626cbec1c1540dc83f1bb99864f5b6
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-15 13:53:53 -0300

    feat: add exist by name method

--- src/adapter/infra/repository/InMemoryRestaurantRepository.ts
+++ src/adapter/infra/repository/InMemoryRestaurantRepository.ts
@@ -2,12 +2,14 @@ import { Restaurant } from '../../../application/domain/Restaurant'
 import { RestaurantRepository } from '../../../application/ports/out/RestaurantRepository'
 
 export class InMemoryRestaurantRepository implements RestaurantRepository {
-
     items = new Array()
 
     async create(restaurant: Restaurant): Promise<Restaurant> {
         this.items.push(restaurant)
         return restaurant
     }
-
+    
+    async existByName(name: string): Promise<boolean> {
+        return this.items.filter(each => each.name === name).length > 0
+    }
 }
