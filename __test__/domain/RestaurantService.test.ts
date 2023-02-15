2f8a3025fc2aaa3ad7ef4d2b10038ef2c4ed9503
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-15 14:39:04 -0300

    test: test add meal to restaurant

--- __test__/domain/RestaurantService.test.ts
+++ __test__/domain/RestaurantService.test.ts
@@ -1,16 +1,21 @@
 import { describe, it, expect, beforeEach } from 'vitest'
+import { InMemoryMealRepository } from '../../src/adapter/infra/repository/InMemoryMealRepository'
 import { InMemoryRestaurantRepository } from '../../src/adapter/infra/repository/InMemoryRestaurantRepository'
 import { RestaurantCreateDTO } from '../../src/application/dto/RestaurantDTO'
 import { RestaurantService } from '../../src/application/ports/in/RestaurantService'
 import { RestaurantServiceImpl } from '../../src/application/service/RestaurantServiceImpl'
+import { Meal } from '../../src/application/domain/Meal'
+import { Restaurant } from '../../src/application/domain/Restaurant'
 
 describe('Restaurant use cases', () => {
 
     const restaurantRepository = new InMemoryRestaurantRepository()
-    const restaurantService: RestaurantService = new RestaurantServiceImpl(restaurantRepository)
+    const mealRepository = new InMemoryMealRepository()
+    const restaurantService: RestaurantService = new RestaurantServiceImpl(restaurantRepository, mealRepository)
 
     beforeEach(() => {
         restaurantRepository.items = new Array()
+        mealRepository.items = new Array()
     })
 
     it('should create a new restaurant', async () => {
@@ -46,5 +51,35 @@ describe('Restaurant use cases', () => {
         await restaurantService.register(restaurantCreateDTO)
 
         expect(restaurantService.register(restaurantCreateDTO)).rejects.toThrow()
+    })
+
+    it('should add a new meal to restaurant', async () => {
+        const restaurantCreateDTO: RestaurantCreateDTO = {
+            name: "Great Restaurant",
+            address: {
+                streetName:"Restaurant street",
+                zipCode: "123321",
+                cityName: "Restaurant city",
+                stateName: "RS"
+            }
+        }
+
+        const meal = new Meal({
+            name: "Pizza",
+            description: "Pizza",
+            price: 30,
+            restaurant: undefined,
+            categories: [
+                "Pizza",
+                "Fast Food"
+            ]
+        })
+
+        mealRepository.items.push(meal)
+        let restaurant = await restaurantService.register(restaurantCreateDTO)
+        restaurant = await restaurantService.addMeal(restaurant.id, meal.id)
+
+        expect(mealRepository.items[0].restaurant).toBeTruthy()
+        expect(restaurant.meals.length).toBe(1)
     })
 })
