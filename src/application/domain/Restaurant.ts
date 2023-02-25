36b75747a4c2b3a2cdbe50fdd072a298a1ca0ff5
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-02-24 21:34:39 -0300

    feat: add meaningful factory method

--- src/application/domain/Restaurant.ts
+++ src/application/domain/Restaurant.ts
@@ -2,10 +2,15 @@ import { v4 as uuid } from 'uuid'
 import { Address } from './Address'
 import { Meal } from './Meal'
 
-type restaurantProperties = {
+type restaurantWithMealProperties = {
     name: string,
     address: Address,
     meals: Meal[]
+}
+
+type restaurantWithoutMealProperties = {
+    name: string,
+    address: Address
 }
 export class Restaurant {
 
@@ -14,10 +19,18 @@ export class Restaurant {
     address: Address
     meals: Meal[]
     
-    constructor(props: restaurantProperties, id?: string) {
+    private constructor(props: restaurantWithMealProperties, id?: string) {
         this.id = id || uuid()
         this.name = props.name
         this.address = props.address
         this.meals = props.meals
+    }
+
+    static withMeal(props: restaurantWithMealProperties, id?: string): Restaurant {
+        return new Restaurant(props, id)
+    } 
+
+    static withoutMeal(props: restaurantWithoutMealProperties, id?: string): Restaurant {
+        return new Restaurant({...props, meals: new Array()}, id)
     }
 }
