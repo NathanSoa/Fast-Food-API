d4e9a735dfa34f5af1d6faa0b7f8dbbe90ebff32
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-03-01 15:36:35 -0300

    feat: remove meal use case

--- src/application/useCases/Restaurant/removeMeal.ts
+++ src/application/useCases/Restaurant/removeMeal.ts
@@ -1,5 +1,32 @@
-import { Meal } from "../../domain/Meal";
+import { Meal } from '../../domain/Meal'
+import { Restaurant } from '../../domain/Restaurant'
+import { MealRepository } from '../../ports/MealRepository'
+import { RestaurantRepository } from '../../ports/RestaurantRepository'
+import { assertMealAndRestaurantExistance } from './util/assertMealAndRestaurantExistance'
 
-export async function removeMeal(): Promise<Meal> {
-    throw new Error("Not implemented yet!")
+export async function removeMeal(
+        restaurantId: string, 
+        mealId: string,
+        restaurantRepository: RestaurantRepository,
+        mealRepository: MealRepository
+    ): Promise<Restaurant> {
+
+        if(!assertMealAndRestaurantExistance(mealId, restaurantId, mealRepository, restaurantRepository)) {
+            throw new Error('Invalid ID was sent!')
+        }
+        
+        const meal = await mealRepository.findById(mealId)
+        const restaurant = await restaurantRepository.findById(restaurantId)
+
+        if(mealExistInRestaurant(meal, restaurant)) {
+            restaurant.meals = restaurant.meals.filter(each => each.id != meal.id)
+            meal.restaurant = undefined
+            restaurantRepository.update(restaurant)
+        }
+
+        return restaurant
+}
+
+function mealExistInRestaurant(meal: Meal, restaurant: Restaurant) {
+   return restaurant.meals.find(eachMeal => eachMeal.id === meal.id) 
 }
