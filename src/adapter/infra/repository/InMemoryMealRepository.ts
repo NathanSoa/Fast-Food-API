4d48eb599db08db70239af69459958a570353feb
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-03-08 16:16:30 -0300

    feat: implemented find filtered feature

--- src/adapter/infra/repository/InMemoryMealRepository.ts
+++ src/adapter/infra/repository/InMemoryMealRepository.ts
@@ -1,12 +1,31 @@
 import { Meal } from '../../../application/domain/Meal'
 import { MealRepository } from '../../../application/ports/MealRepository'
+import { mealFilterParams } from '../../../application/useCases/Customer/findMeal'
 
 export class InMemoryMealRepository implements MealRepository {
     
-    items = new Array()
+    items: Meal[] = new Array()
     
-    findById(id: string): Promise<Meal> {
-        return this.items.find(each => each.id === id)
+    async findById(id: string): Promise<Meal | undefined> {
+       return this.items.find(each => each.id === id)
     }
 
+    async findFiltered(filters: mealFilterParams): Promise<Meal[]> {
+        let meals = new Array()
+                    
+        if(filters.name) {
+            meals = this.items.filter(each => each.description === filters.name)
+        }
+
+        if(filters.restaurant) {
+            meals = this.items.filter(each => each.restaurant?.name === filters.restaurant)
+        }
+
+        if(filters.maxPrice) {
+            //@ts-ignore
+            meals = meals.filter(each => each.price <= filters.maxPrice)
+        }
+
+        return meals
+    }
 }
