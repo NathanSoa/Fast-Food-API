6bba8e29ddc592893b9d17f913f9d752471ea216
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-03-08 16:16:15 -0300

    feat: update repository

--- src/application/ports/MealRepository.ts
+++ src/application/ports/MealRepository.ts
@@ -1,5 +1,7 @@
 import { Meal } from '../domain/Meal'
+import { mealFilterParams } from '../useCases/Customer/findMeal'
 export interface MealRepository {
     
-    findById(id: string): Promise<Meal>
+    findById(id: string): Promise<Meal | undefined>
+    findFiltered(filters: mealFilterParams): Promise<Meal[]>
 }
