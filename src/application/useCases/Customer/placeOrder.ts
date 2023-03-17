ee0ee8a6124be61799b58075b24e39919fea99c9
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-03-17 19:23:07 -0300

    refactor: function now use the calculate method

--- src/application/useCases/Customer/placeOrder.ts
+++ src/application/useCases/Customer/placeOrder.ts
@@ -1,16 +1,19 @@
 import { Order } from '../../domain/Order'
 import { PlaceNewOrderDTO } from '../../dto/OrderDTO'
+import { MealRepository } from '../../ports/MealRepository'
 import { OrderRepository } from '../../ports/OrderRepository'
 import { RestaurantRepository } from '../../ports/RestaurantRepository'
 import { assertMealItemsAreCorrect } from './util/assertMealItemsAreCorrect'
+import { calculateOrderTotal } from './util/caculateOrderTotal'
 
 export async function placeOrder(
     orderDTO: PlaceNewOrderDTO,
     restaurantRepository: RestaurantRepository,
-    orderRepository: OrderRepository
+    orderRepository: OrderRepository,
+    mealRepository: MealRepository
 ) {
 
     await assertMealItemsAreCorrect(orderDTO.restaurantId, orderDTO.orderItems, restaurantRepository)
-
-    return await orderRepository.save(Order.NewOne({...orderDTO}))
+    const total = await calculateOrderTotal(mealRepository, orderDTO.orderItems)
+    return await orderRepository.save(Order.NewOne({...orderDTO, total}))
 }
