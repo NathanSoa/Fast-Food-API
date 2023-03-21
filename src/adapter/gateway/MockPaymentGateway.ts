1e5a4a5ad9c502c07abfcac0bc9a48dc4818d0f9
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2023-03-21 16:14:56 -0300

    feat: add failure mock

--- src/adapter/gateway/MockPaymentGateway.ts
+++ src/adapter/gateway/MockPaymentGateway.ts
@@ -3,6 +3,14 @@ import { PaymentGateway } from '../../application/ports/PaymentGateway'
 export class MockPaymentGateway implements PaymentGateway {
 
     async process(amount: number, cardNumber: string): Promise<boolean> {
-        return true;
+        return true
     }
+}
+
+export class ErrorMockPaymentGateway implements PaymentGateway {
+    
+    async process(amount: number, cardNumber: string): Promise<boolean> {
+        return false
+    }
+
 }
